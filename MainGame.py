520997a03c911a3209a1858444a178a0a89755ce
TheBorgQueen21 <67753887+TheBorgQueen21@users.noreply.github.com>
2020-07-20 18:30:40 -0400

    anika: continued changes

--- MainGame.py
+++ MainGame.py
@@ -32,9 +32,42 @@ playerY_change = 0
 def player(x, y):
     screen.blit(playerImg, (x, y)) #draws the player on the screen
 
-#Screen One Item Counter
+#Trying out Classes for Different Rooms
+"""class Room(object):
+    Base class for all rooms.
 
+    # Each room has a list of walls, and of enemy sprites.
+    background_list = []
+    sprite_list = None
 
+    def __init__(self):
+        Constructor, create our lists.
+        self.background_list = pygame.sprite.Group()
+        self.sprite_list = pygame.sprite.Group()
+
+class Room1(Room):
+    This creates all the walls in room 1
+    def __init__(self):
+        super().__init__()
+        # Make the walls. (x_pos, y_pos, width, height)
+
+        # This is a list of walls. Each is in the form [x, y, width, height]
+        sprites = [[playerX == 143.5, playerY<=480, playerY >=402.5], #playerX == 143.5 and playerY<=480 and playerY >=402.5
+                 [playerX==143.5, playerY<=334, playerY >=262.5], #playerX==143.5 and playerY<=334 and playerY >=262.5
+                 [playerX>=143.5, playerX<=247, playerY == 64], #playerX>=143.5 and playerX<=247 and playerY == 64
+                 [playerY==64, playerX>=494.5, playerX<=562.5], #playerY==64 and playerX>=494.5 and playerX<=562.5
+                 [playerX==630.5, playerY>=225.5, playerY <=291], #playerX==630.5 and playerY>=225.5 and playerY <=291
+                 [playerX== 630.5, playerY>=375.5, playerY<= 480], #playerX== 630.5 and playerY>=375.5 and playerY<= 480
+                ]
+        background.append(screen_one_background)
+
+class Room2(Room):
+    This creates all the walls in room 1
+    def __init__(self):
+        super().__init__()"""
+        # Make the walls. (x_pos, y_pos, width, height)
+
+scenerealnum = 0
 #Press space
 def spacepress (scripttext, secondtext):
     #global sone_item_count
@@ -47,20 +80,21 @@ def spacepress (scripttext, secondtext):
             screen.blit (second_text, (20, 450))
             #sone_item_count += 1
 
-
 def screen_two ():
     playerX = 75
     playerY = 300
     screen.blit(screen_two_background, (0, 0)) #figrue out how to change the backgrounds
+
+
 def screen_three ():
     playerX = 75
     playerY = 300
     screen.blit(screen_three_background, (0, 0)) #figrue out how to change the backgrounds
 
 #Screen 1-at home
-def screen_one ():
+def screen_one (scenenum):
     screen.blit(screen_one_background, (0, 0))
-    screentwo = False
+    scenenum = 1
     if playerX == 143.5 and playerY<=480 and playerY >=402.5:
         print("plant")
         spacepress("plant_Trying out code", "")
@@ -69,9 +103,9 @@ def screen_one ():
         spacepress("phone_Trying out code" , "")
     elif playerX>=143.5 and playerX<=247 and playerY == 64:
         print("painting")
-        spacepress("This is a painting that came with this house.", "There is something behind it. (press e to take out painting)")
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_e:
+        spacepress("This is a painting that came with this house.", "There is something behind it. Press E to Go through")
+        #if event.type == pygame.KEYUP:
+            #if event.key == pygame.K_e:
 
     elif playerY==64 and playerX>=494.5 and playerX<=562.5:
         print("piggybank")
@@ -116,19 +150,42 @@ while running: #anything you want to appear continuously must go inside this whi
     #Keeps Player in Bounds
     playerX += playerX_change
     playerY += playerY_change
-    if playerX <= 143.5:
-        playerX = 143.5
-    elif playerX >= 630.5:#736
-        playerX = 630.5
-    if playerY <= 64:
-        playerY = 64
-    elif playerY >= 480:
-        playerY = 480
+
     #print (playerX)
     #print (playerY)
 
+    #if event.type == pygame.KEYUP:
+        #if event.key == pygame.K_e:
+    # playerX>=143.5 and playerX<=247 and playerY == 64
+
+
     #background mage
     screen.fill((0, 0, 0))
-    screen_one()
+    screen_one(scenerealnum)
     player(playerX, playerY)
+
+    if not scenerealnum == 1:
+        if playerX <= 143.5:
+            playerX = 143.5
+        elif playerX >= 630.5:#736
+            playerX = 630.5
+        if playerY <= 64:
+            playerY = 64
+        elif playerY >= 480:
+            playerY = 480
+        print ("not scene 1")
+
+    elif scenerealnum == 1:
+        if playerX <= 247:
+            playerX = 247
+        elif playerX >= 630.5:
+            playerX = 630.5
+        if playerY <= 64:
+            playerY = 64
+        elif playerY >= 480:
+            playerY = 480
+        if playerX>=143.5 and playerX<=247 and playerY == 64:
+            screen_two()
+        print("scene 1")
+
     pygame.display.update()
