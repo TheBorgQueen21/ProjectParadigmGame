09c9460d7753f8322c4ffe014524eb9339381eca
TheBorgQueen21 <67753887+TheBorgQueen21@users.noreply.github.com>
2020-08-19 18:07:54 -0400

    Update FixingMainGame.py

--- FixingMainGame.py
+++ FixingMainGame.py
@@ -14,6 +14,7 @@ ORANGE = (255, 128, 0)
 LIGHTPURPLE = (229, 204, 225)
 LIGHTPINK = (255, 204, 255)
 LIGHTBLUE = (204, 255, 255)
+PEACH = (255,228,181)
 
 
 game_font = pygame.font.Font('freesansbold.ttf', 15)
@@ -42,7 +43,7 @@ class Wall(pygame.sprite.Sprite):
 class Object(pygame.sprite.Sprite):
     """This class represents the bar at the bottom that the player controls """
  
-    def __init__(self, x, y, width, height, color):
+    def __init__(self, x, y, width, height, color, toptext, bottomtext):
         """ Constructor function """
  
         # Call the parent's constructor
@@ -53,6 +54,8 @@ class Object(pygame.sprite.Sprite):
         self.image.fill(color)
         self.width = width
         self.height = height
+        self.toptext = toptext
+        self.bottomtext = bottomtext
  
         # Make our top-left corner the passed-in location.
         self.rect = self.image.get_rect()
@@ -66,8 +69,8 @@ class Object(pygame.sprite.Sprite):
     
     def hit(self):
       pygame.draw.rect(screen, WHITE, (0, 350, 800, 600))
-      first_text = game_font.render("Hello! My name is Anika", True, BLUE)
-      second_text= game_font.render("I hope this works!", True, BLUE)
+      first_text = game_font.render(self.toptext, True, BLUE)
+      second_text= game_font.render(self.bottomtext, True, BLUE)
       screen.blit (first_text, (20, 400))
       screen.blit (second_text, (20, 450))
       
@@ -76,6 +79,45 @@ class Object(pygame.sprite.Sprite):
         if playerx + player2 > self.hitbox[0] and playerx < self.hitbox[0] + self.hitbox[2]:
           self.hit()
 
+class Sprite (pygame.sprite.Sprite):
+    """This class represents the bar at the bottom that the player controls """
+ 
+    def __init__(self, x, y, width, height, color, toptext, bottomtext):
+        """ Constructor function """
+ 
+        # Call the parent's constructor
+        super().__init__()
+ 
+        # Make a BLUE wall, of the size specified in the parameters
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+        self.width = width
+        self.height = height
+        self.toptext = toptext
+        self.bottomtext = bottomtext
+ 
+        # Make our top-left corner the passed-in location.
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
+        self.hitbox = (self.rect.x-20, self.rect.y-15, self.width+10, self.height+15) # NEW
+
+    def draw_hitbox(self, x, y, width, height):
+      self.hitbox = (self.rect.x-20, self.rect.y-15, self.width+10, self.height+15) # NEW
+      pygame.draw.rect(screen, (255,0,0), self.hitbox,2) # To draw the hit box around the player
+    
+    def hit(self):
+      pygame.draw.rect(screen, WHITE, (0, 350, 800, 600))
+      first_text = game_font.render(self.toptext, True, BLUE)
+      second_text= game_font.render(self.bottomtext, True, BLUE)
+      screen.blit (first_text, (20, 400))
+      screen.blit (second_text, (20, 450))
+      
+    def hit_hitbox (self, playerx, playery, player2, player3):
+      if playery < self.hitbox[1] + self.hitbox[3] and playery + player3 > self.hitbox[1]:
+        if playerx + player2 > self.hitbox[0] and playerx < self.hitbox[0] + self.hitbox[2]:
+          self.hit()
+ 
  
 class Player(pygame.sprite.Sprite):
     """ This class represents the bar at the bottom that the
@@ -113,7 +155,7 @@ class Player(pygame.sprite.Sprite):
         self.change_x += x
         self.change_y += y
  
-    def move(self, walls, objects): ###added objects here
+    def move(self, walls, objects, sprites): ###added objects here
         """ Find a new position for the player """
  
         # Move left/right
@@ -164,6 +206,28 @@ class Player(pygame.sprite.Sprite):
                 self.rect.bottom = block.rect.top
             else:
                 self.rect.top = block.rect.bottom
+         
+        ###---NPC Sprites ---###      
+        #Did this update cause us to hit a wall?
+        block_hit_list = pygame.sprite.spritecollide(self, sprites, False)
+
+        for block in block_hit_list:
+            # If we are moving right, set our right side to the left side of
+            # the item we hit
+            if self.change_x > 0:
+                self.rect.right = block.rect.left
+            else:
+                # Otherwise if we are moving left, do the opposite.
+                self.rect.left = block.rect.right
+ 
+        # Check and see if we hit anything
+        block_hit_list = pygame.sprite.spritecollide(self, sprites, False)
+        for block in block_hit_list:
+            # Reset our position based on the top/bottom of the object.
+            if self.change_y > 0:
+                self.rect.bottom = block.rect.top
+            else:
+                self.rect.top = block.rect.bottom
         
  #####***JULY 24: I need to fix the walls and where they are placed. I already: Converted the code above, added all the rooms (the rooms don't have correct walls yet)***#####
  #####***JULY 27: I need to fix the walls' placement. I already: Fixed the order of the rooms ****######
@@ -174,6 +238,9 @@ class Player(pygame.sprite.Sprite):
  #####***AUGUST 12: I need to find out how to access the arguments from a class outside the class (so that way I can compare the in game objects to the player's x and y)
  #####***AUGUST 13: I am in the middle of making hitboxes. I finished making hitboxes for the player, trying to figure out how to do it for the objects
  #####***AUGUST 14: I finished making the hitboxes for the player and objects!! Now I need add what happens when they collide
+ #####***AUGUST 19: I finished adding the collisions for hitboxes and the text comes up when collided
+ ################## I need to figure out how to make text appear and disappear when pressed.
+ 
  
 player = Player(50, 50)
  
@@ -214,16 +281,23 @@ class Room_Bedroom (Room): #finished walls
         
         
     def draw (self):
-        objects = [[200, 200, 40, 60, BLUE], 
-                    [500, 300, 100, 80, GREEN] 
+        objects = [[200, 200, 40, 60, BLUE, "This is the bedroom", "This is the bed"], 
+                    [500, 300, 100, 80, GREEN, "This is the bedroom", "This is the desk"] 
                   ]
         for item in objects:
-            game_object = Object(item[0], item[1], item[2], item[3], item[4])
+            game_object = Object(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
             self.object_list.add(game_object) 
             game_object.draw_hitbox(item[0], item[1], item[2], item[3])
             game_object.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3])    
                 
-
+        sprites = [[200, 300, 50, 50, PEACH, "This is a character", "Hello!!"]
+                  ]
+        for item in sprites:
+            npc_Sprite = Sprite(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
+            self.chara_sprites.add(npc_Sprite) 
+            npc_Sprite.draw_hitbox(item[0], item[1], item[2], item[3])
+            npc_Sprite.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3])    
+                
  
 class Room_Cave1(Room): #walls done
     """This creates all the walls in room 2"""
@@ -246,13 +320,21 @@ class Room_Cave1(Room): #walls done
             
             
     def draw (self):
-        objects = [[200, 200, 40, 60, BLUE]] 
+        objects = [[200, 200, 40, 60, BLUE, "text here", "wow, this works!!"]] 
         for item in objects:
-            game_object = Object(item[0], item[1], item[2], item[3], item[4])
+            game_object = Object(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
             self.object_list.add(game_object) 
             game_object.draw_hitbox(item[0], item[1], item[2], item[3])
             game_object.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3]) 
             
+        sprites = [[200, 300, 20, 20, PEACH, "This is a character", "Hello!!"]
+                  ]
+        for item in sprites:
+            npc_Sprite = Sprite(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
+            self.chara_sprites.add(npc_Sprite) 
+            npc_Sprite.draw_hitbox(item[0], item[1], item[2], item[3])
+            npc_Sprite.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3])    
+                
 class Room_Cave2 (Room): #walls done
     """This creates all the walls in room 3"""
     def __init__(self):
@@ -272,13 +354,21 @@ class Room_Cave2 (Room): #walls done
             self.wall_list.add(wall)
             
     def draw (self):
-        objects = [[200, 200, 40, 60, BLUE]] 
+        objects = [[200, 200, 40, 60, BLUE, "text here", "wow, this works!!"]] 
         for item in objects:
-            game_object = Object(item[0], item[1], item[2], item[3], item[4])
+            game_object = Object(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
             self.object_list.add(game_object) 
             game_object.draw_hitbox(item[0], item[1], item[2], item[3])
             game_object.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3]) 
-           
+
+        sprites = [[200, 300, 20, 20, PEACH, "This is a character", "Hello!!"]
+                  ]
+        for item in sprites:
+            npc_Sprite = Sprite(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
+            self.chara_sprites.add(npc_Sprite) 
+            npc_Sprite.draw_hitbox(item[0], item[1], item[2], item[3])
+            npc_Sprite.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3])    
+                           
 class Room_Cave3 (Room): #finsihed these walls
     """This creates all the walls in room 3"""
     def __init__(self):
@@ -298,13 +388,21 @@ class Room_Cave3 (Room): #finsihed these walls
 
             
     def draw (self):
-        objects = [[200, 200, 40, 60, BLUE]] 
+        objects = [[200, 200, 40, 60, BLUE, "text here", "wow, this works!!"]] 
         for item in objects:
-            game_object = Object(item[0], item[1], item[2], item[3], item[4])
+            game_object = Object(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
             self.object_list.add(game_object) 
             game_object.draw_hitbox(item[0], item[1], item[2], item[3])
             game_object.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3]) 
- 
+
+        sprites = [[200, 300, 20, 20, PEACH, "This is a character", "Hello!!"]
+                  ]
+        for item in sprites:
+            npc_Sprite = Sprite(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
+            self.chara_sprites.add(npc_Sprite) 
+            npc_Sprite.draw_hitbox(item[0], item[1], item[2], item[3])
+            npc_Sprite.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3])    
+                 
  
 class Room_WorldOne (Room):
     """This creates all the walls in room 3"""
@@ -324,13 +422,21 @@ class Room_WorldOne (Room):
             self.wall_list.add(wall)
             
     def draw (self):
-        objects = [[200, 200, 40, 60, BLUE]] 
+        objects = [[200, 200, 40, 60, BLUE, "text here", "wow, this works!!"]] 
         for item in objects:
-            game_object = Object(item[0], item[1], item[2], item[3], item[4])
+            game_object = Object(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
             self.object_list.add(game_object) 
             game_object.draw_hitbox(item[0], item[1], item[2], item[3])
             game_object.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3])    
-        
+
+        sprites = [[200, 300, 20, 20, PEACH, "This is a character", "Hello!!"]
+                  ]
+        for item in sprites:
+            npc_Sprite = Sprite(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
+            self.chara_sprites.add(npc_Sprite) 
+            npc_Sprite.draw_hitbox(item[0], item[1], item[2], item[3])
+            npc_Sprite.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3])    
+                        
             
 class Room_WorldTwo (Room):
     """This creates all the walls in room 3"""
@@ -349,13 +455,22 @@ class Room_WorldTwo (Room):
             self.wall_list.add(wall)            
  
     def draw (self):
-        objects = [[200, 200, 40, 60, BLUE]] 
+        objects = [[200, 200, 40, 60, BLUE, "text here", "wow, this works!!"]] 
         for item in objects:
-            game_object = Object(item[0], item[1], item[2], item[3], item[4])
+            game_object = Object(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
             self.object_list.add(game_object) 
             game_object.draw_hitbox(item[0], item[1], item[2], item[3])
             game_object.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3]) 
-            
+
+        sprites = [[200, 300, 20, 20, PEACH, "This is a character", "Hello!!"]
+                  ]
+        for item in sprites:
+            npc_Sprite = Sprite(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
+            self.chara_sprites.add(npc_Sprite) 
+            npc_Sprite.draw_hitbox(item[0], item[1], item[2], item[3])
+            npc_Sprite.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3])    
+                
+                            
 class Room_WorldThree (Room):
     """This creates all the walls in room 3"""
     def __init__(self):
@@ -375,13 +490,22 @@ class Room_WorldThree (Room):
             
             
     def draw (self):
-        objects = [[200, 200, 40, 60, BLUE]] 
+        objects = [[200, 200, 40, 60, BLUE, "text here", "wow, this works!!"]] 
         for item in objects:
-            game_object = Object(item[0], item[1], item[2], item[3], item[4])
+            game_object = Object(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
             self.object_list.add(game_object) 
             game_object.draw_hitbox(item[0], item[1], item[2], item[3])
             game_object.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3]) 
 
+        sprites = [[200, 300, 20, 20, PEACH, "This is a character", "Hello!!"]
+                  ]
+        for item in sprites:
+            npc_Sprite = Sprite(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
+            self.chara_sprites.add(npc_Sprite) 
+            npc_Sprite.draw_hitbox(item[0], item[1], item[2], item[3])
+            npc_Sprite.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3])    
+                
+                
 class Room_WorldFour (Room):
     """This creates all the walls in room 3"""
     def __init__(self):
@@ -399,13 +523,23 @@ class Room_WorldFour (Room):
             self.wall_list.add(wall)
             
     def draw (self):
-        objects = [[200, 200, 40, 60, BLUE]] 
+        objects = [[200, 200, 40, 60, BLUE, "text here", "wow, this works!!"]] 
         for item in objects:
-            game_object = Object(item[0], item[1], item[2], item[3], item[4])
+            game_object = Object(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
             self.object_list.add(game_object) 
             game_object.draw_hitbox(item[0], item[1], item[2], item[3])
             game_object.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3]) 
 
+        sprites = [[200, 300, 20, 20, PEACH, "This is a character", "Hello!!"]
+                  ]
+        for item in sprites:
+            npc_Sprite = Sprite(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
+            self.chara_sprites.add(npc_Sprite) 
+            npc_Sprite.draw_hitbox(item[0], item[1], item[2], item[3])
+            npc_Sprite.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3])    
+                
+                
+                
 class Room_WorldFive (Room):
     """This creates all the walls in room 3"""
     def __init__(self):
@@ -424,13 +558,22 @@ class Room_WorldFive (Room):
             self.wall_list.add(wall)
  
     def draw (self):
-        objects = [[200, 200, 40, 60, BLUE]] 
+        objects = [[200, 200, 40, 60, BLUE, "text here", "wow, this works!!"]] 
         for item in objects:
-            game_object = Object(item[0], item[1], item[2], item[3], item[4])
+            game_object = Object(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
             self.object_list.add(game_object) 
             game_object.draw_hitbox(item[0], item[1], item[2], item[3])
             game_object.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3]) 
-            
+
+        sprites = [[200, 300, 20, 20, PEACH, "This is a character", "Hello!!"]
+                  ]
+        for item in sprites:
+            npc_Sprite = Sprite(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
+            self.chara_sprites.add(npc_Sprite) 
+            npc_Sprite.draw_hitbox(item[0], item[1], item[2], item[3])
+            npc_Sprite.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3])    
+                
+                            
             
 class Room_WorldSix (Room):
   """This creates all the walls in room 3"""
@@ -449,13 +592,22 @@ class Room_WorldSix (Room):
       self.wall_list.add(wall)
       
   def draw (self):
-      objects = [[200, 200, 40, 60, BLUE]] 
+      objects = [[200, 200, 40, 60, BLUE, "text here", "wow, this works!!"]] 
       for item in objects:
-          game_object = Object(item[0], item[1], item[2], item[3], item[4])
+          game_object = Object(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
           self.object_list.add(game_object) 
           game_object.draw_hitbox(item[0], item[1], item[2], item[3])
           game_object.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3]) 
- 
+
+      sprites = [[200, 300, 20, 20, PEACH, "This is a character", "Hello!!"]
+                  ]
+      for item in sprites:
+          npc_Sprite = Sprite(item[0], item[1], item[2], item[3], item[4], item[5], item[6])
+          self.chara_sprites.add(npc_Sprite) 
+          npc_Sprite.draw_hitbox(item[0], item[1], item[2], item[3])
+          npc_Sprite.hit_hitbox(player.hitbox[0], player.hitbox[1], player.hitbox[2], player.hitbox[3])    
+                
+                 
 def main():
     """ Main Program """
  
@@ -545,7 +697,7 @@ def main():
                     player.changespeed(0, -5)
  
         # --- Game Logic ---
-        player.move(current_room.wall_list, current_room.object_list)
+        player.move(current_room.wall_list, current_room.object_list, current_room.chara_sprites)
       
         if player.rect.x < -15:
             if current_room_no == 1:
@@ -610,7 +762,7 @@ def main():
                 current_room_no = 1
                 current_room = rooms[current_room_no]
                 player.rect.y = 550
-            elif current_room_no == 1: ##Something wrong here
+            elif current_room_no == 1: 
                 current_room_no = 2
                 current_room = rooms[current_room_no]
                 player.rect.y = 550
