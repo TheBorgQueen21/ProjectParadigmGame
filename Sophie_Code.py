3129d46383a4e751e0d634b4d7591405310f46cf
sophiewojcicki <68028952+sophiewojcicki@users.noreply.github.com>
2020-08-23 13:47:32 -0400

    Update Sophie_Code.py

--- Sophie_Code.py
+++ Sophie_Code.py
@@ -1,22 +1,34 @@
 import pygame
-
+import time
 pygame.init()
 
 green = (0,255,0)
 black = (0,0,0)
+red = (255,0,0)
 
-dis = pygame.display.set_mode((800,600)) # This is the screen size, it can be changed if needed
+dis_width = 800 # screen width
+dis_height = 600 # screen height
+dis = pygame.display.set_mode((dis_width,dis_height)) # This is the screen size, it can be changed if needed
 pygame.display.set_caption('Snake Game') # This doesn't display "Snake Game", idk why but its not needed anyways
 
 game_over = False
 
-x1 = 300
-y1 = 300
+x1 = dis_width/2
+y1 = dis_height/2
 
 x1_change = 0
 y1_change = 0
 
+snake_block = 10
+snake_speed = 30
+
 clock = pygame.time.Clock()
+
+font_style = pygame.font.SysFont(None, 50)
+
+def message(msg,color):
+  mesg = font_style.render(msg, True, color)
+  dis.blit(mesg, [dis_width/2, dis_height/2])
 
 while not game_over:
   for event in pygame.event.get(): # Records where mouse is located I think
@@ -36,6 +48,9 @@ while not game_over:
         y1_change = 10
         x1_change = 0
   
+  if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+    game_over = True
+  
   x1 += x1_change
   y1 += y1_change
   dis.fill(black) # changes background color
@@ -43,7 +58,11 @@ while not game_over:
   
   pygame.display.update()
   
-  clock.tick(30)
-  
+  clock.tick(snake_speed)
+
+message("Game Over",red)
+pygame.display.update()
+time.sleep(2) # Screen stays for 2 seconds I think
+
 pygame.quit
 quit()
