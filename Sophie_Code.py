4a3bbd2a804e42c5d971894cc834e1aa482b5b31
sophiewojcicki <68028952+sophiewojcicki@users.noreply.github.com>
2020-08-22 23:59:55 -0400

    Update Sophie_Code.py

--- Sophie_Code.py
+++ Sophie_Code.py
@@ -1,4 +1,5 @@
 import pygame
+
 pygame.init()
 
 green=(0,255,0)
@@ -21,19 +22,19 @@ while not game_over:
   for event in pygame.event.get(): # Records where mouse is located
     if event.type == pygame.QUIT: # This line and the next might not be needed, they allow the user to close the screen when the mouse is over the quit button, but there's not quite button so im confused
       game_over==True
-     if event.type == pygame.KEYDOWN: # When the user presses a key, the following code controls the snake's direction
-      if event.key == pygame.K_LEFT:
-        x1_change = -10
-        y1_change = 0
-      if event.key == pygame.K_RIGHT:
-        x1_change = 10
-        y1_change = 0
-      if event.key == pygame.K_UP:
-        x1_change = -10
-        y1_change = 0
-      if event.key == pygame.K_DOWN:
-        x1_change = 10
-        y1_change = 0
+      if event.type == pygame.KEYDOWN: # When the user presses a key, the following code controls the snake's direction
+        if event.key == pygame.K_LEFT:
+          x1_change = -10
+          y1_change = 0
+        if event.key == pygame.K_RIGHT:
+          x1_change = 10
+          y1_change = 0
+        if event.key == pygame.K_UP:
+          x1_change = -10
+          y1_change = 0
+        if event.key == pygame.K_DOWN:
+          x1_change = 10
+          y1_change = 0
   
   x1 += x1_change
   y1 += y1_change
