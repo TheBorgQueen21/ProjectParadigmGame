9d1039cc3363c5e752fdeb415f5fd9f973eef5f2
sophiewojcicki <68028952+sophiewojcicki@users.noreply.github.com>
2020-08-22 23:54:18 -0400

    Update Sophie_Code.py

--- Sophie_Code.py
+++ Sophie_Code.py
@@ -1,18 +1,48 @@
 import pygame
 pygame.init()
-dis=pygame.display.set_mode((400,300)) # This is the screen size, it can be changed if needed
-# I had pygame.display.update() here but idk if it's still needed
-
-pygame.display.set_caption('Snake Game') # This doesn't display "Snake Game", idk why but its not needed anyways
 
 green=(0,255,0)
 blue=(0,0,225)
 
+dis=pygame.display.set_mode((800,600)) # This is the screen size, it can be changed if needed
+pygame.display.set_caption('Snake Game') # This doesn't display "Snake Game", idk why but its not needed anyways
+
 game_over=False
+
+x1 = 300
+y1 = 300
+
+x1_change = 0
+y1_change = 0
+
+clock = pygame.time.Clock()
+
 while not game_over:
   for event in pygame.event.get(): # Records where mouse is located
-    if event.type==pygame.QUIT: # This line and the next might not be needed, they allow the user to close the screen when the mouse is over the quit button, but there's not quite button so im confused
+    if event.type == pygame.QUIT: # This line and the next might not be needed, they allow the user to close the screen when the mouse is over the quit button, but there's not quite button so im confused
       game_over==True
-    
+     if event.type == pygame.KEYDOWN: # When the user presses a key, the following code controls the snake's direction
+      if event.key == pygame.K_LEFT:
+        x1_change = -10
+        y1_change = 0
+      if event.key == pygame.K_RIGHT:
+        x1_change = 10
+        y1_change = 0
+      if event.key == pygame.K_UP:
+        x1_change = -10
+        y1_change = 0
+      if event.key == pygame.K_DOWN:
+        x1_change = 10
+        y1_change = 0
+  
+  x1 += x1_change
+  y1 += y1_change
+  # to change background color put dis.fill(color) make sure the color is defined earlier in code
+  pygame.draw.rect(dis, green, [x1, y1, 10, 10])
+  
+  pygame.display.update()
+  
+  clock.tick(30)
+  
 pygame.quit
 quit()
