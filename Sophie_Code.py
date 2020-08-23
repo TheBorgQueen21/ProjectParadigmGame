d4a155c0468a3ba6765f0e1403ef95c3997c1973
sophiewojcicki <68028952+sophiewojcicki@users.noreply.github.com>
2020-08-23 14:44:49 -0400

    Update Sophie_Code.py

--- Sophie_Code.py
+++ Sophie_Code.py
@@ -1,10 +1,13 @@
 import pygame
 import time
+import random
+
 pygame.init()
 
 green = (0,255,0)
 black = (0,0,0)
 red = (255,0,0)
+white = (255,255,255)
 
 dis_width = 800 # screen width
 dis_height = 600 # screen height
@@ -12,12 +15,6 @@ dis = pygame.display.set_mode((dis_width,dis_height)) # This is the screen size,
 pygame.display.set_caption('Snake Game') # This doesn't display "Snake Game", idk why but its not needed anyways
 
 game_over = False
-
-x1 = dis_width/2
-y1 = dis_height/2
-
-x1_change = 0
-y1_change = 0
 
 snake_block = 10
 snake_speed = 30
@@ -30,39 +27,69 @@ def message(msg,color):
   mesg = font_style.render(msg, True, color)
   dis.blit(mesg, [dis_width/2, dis_height/2])
 
-while not game_over:
-  for event in pygame.event.get(): # Records where mouse is located I think
-    if event.type == pygame.QUIT: # This line and the next might not be needed, they allow the user to close the screen when the mouse is over the quit button, but there's not quite button so im confused
-      game_over == True
-    if event.type == pygame.KEYDOWN: # When the user presses a key, the following code controls the snake's direction
-      if event.key == pygame.K_LEFT:
-        x1_change = -10
-        y1_change = 0
-      elif event.key == pygame.K_RIGHT:
-        x1_change = 10
-        y1_change = 0
-      elif event.key == pygame.K_UP:
-        y1_change = -10
-        x1_change = 0
-      elif event.key == pygame.K_DOWN:
-        y1_change = 10
-        x1_change = 0
+def gameLoop(): # Creates a function
+  game_over = False
+  game_close = False
   
-  if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-    game_over = True
+  x1 = dis_width / 2
+  y1 = dis_height / 2
+
+  x1_change = 0
+  y1_change = 0
   
-  x1 += x1_change
-  y1 += y1_change
-  dis.fill(black) # changes background color
-  pygame.draw.rect(dis, green, [x1, y1, 10, 10])
+  foodx = round(random.randrange(0, dis_width - snake_block) / 10.0)
+  foody = round(random.randrange(0, dis_width - snake_block) / 10.0)
+
+  while not game_over:
+    while game_close == True:
+      dis.fill(black) # Changes the background color
+      message("You Lost! Press Q to Quit or C to Play Again.", red)
+      pygame.display.update()
+    
+      for event in pygame.event.get():
+        if. event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_q: # If the user presses Q
+            game_over = True
+            game_close = False
+          if event.key == pygame.K_c: # If the user presses C
+            gameLoop()
+    
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT: # This line and the next might not be needed, they allow the user to close the screen when the mouse is over the quit button, but there's not quite button so im confused
+        game_over == True
+      if event.type == pygame.KEYDOWN: # When the user presses a direction key, the following code controls the snake's direction
+        if event.key == pygame.K_LEFT:
+          x1_change = -snake_block
+          y1_change = 0
+        elif event.key == pygame.K_RIGHT:
+          x1_change = snake_block
+          y1_change = 0
+        elif event.key == pygame.K_UP:
+          y1_change = -snake_block
+          x1_change = 0
+        elif event.key == pygame.K_DOWN:
+          y1_change = snake_block
+          x1_change = 0
   
+    if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+      game_over = True
+  
+    x1 += x1_change
+    y1 += y1_change
+    dis.fill(black) # changes background color
+    pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
+    pygame.draw.rect(dis, white, [x1, y1, snake_block, snake_block])
+    pygame.display.update()
+    
+    if x1 == foodx and y1 == foody:
+      print("Yum!")
+    clock.tick(snake_speed)
+
+  message("Game Over",red)
   pygame.display.update()
+  time.sleep(2) # Game over screen stays for 2 seconds I think
+
+  pygame.quit
+  quit()
   
-  clock.tick(snake_speed)
-
-message("Game Over",red)
-pygame.display.update()
-time.sleep(2) # Screen stays for 2 seconds I think
-
-pygame.quit
-quit()
+gameLoop()
