929c3b6217c4cbca5fc41d71990cf7f8287281df
sophiewojcicki <68028952+sophiewojcicki@users.noreply.github.com>
2020-08-23 15:22:37 -0400

    Update Sophie_Code.py

--- Sophie_Code.py
+++ Sophie_Code.py
@@ -18,14 +18,14 @@ pygame.display.set_caption('Snake Game') # This doesn't display "Snake Game", id
 clock = pygame.time.Clock()
 
 snake_block = 10
-snake_speed = 30
+snake_speed = 15
 
-font_style = pygame.font.SysFont(None, 30)
-
+font_style = pygame.font.SysFont(None, 50)
+score_font = pygame.font.SysFont("comicsansms", 35)
 
 def message(msg, color):
   mesg = font_style.render(msg, True, color)
-  dis.blit(mesg, [dis_width/3, dis_height/3])
+  dis.blit(mesg, [dis_width/15, dis_height/3]) # Controls where messages appear
 
   
 def gameLoop(): # Creates a function
