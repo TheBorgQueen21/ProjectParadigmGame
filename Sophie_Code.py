91d48b49f61cfcebf9bddd1d9130730bd3c60d99
sophiewojcicki <68028952+sophiewojcicki@users.noreply.github.com>
2020-08-23 15:05:50 -0400

    Update Sophie_Code.py

--- Sophie_Code.py
+++ Sophie_Code.py
@@ -11,22 +11,23 @@ white = (255,255,255)
 
 dis_width = 800 # screen width
 dis_height = 600 # screen height
+
 dis = pygame.display.set_mode((dis_width,dis_height)) # This is the screen size, it can be changed if needed
 pygame.display.set_caption('Snake Game') # This doesn't display "Snake Game", idk why but its not needed anyways
 
-game_over = False
+clock = pygame.time.Clock()
 
 snake_block = 10
 snake_speed = 30
 
-clock = pygame.time.Clock()
+font_style = pygame.font.SysFont(None, 30)
 
-font_style = pygame.font.SysFont(None, 50)
 
-def message(msg,color):
+def message(msg, color):
   mesg = font_style.render(msg, True, color)
-  dis.blit(mesg, [dis_width/2, dis_height/2])
+  dis.blit(mesg, [dis_width/3, dis_height/3])
 
+  
 def gameLoop(): # Creates a function
   game_over = False
   game_close = False
@@ -37,17 +38,18 @@ def gameLoop(): # Creates a function
   x1_change = 0
   y1_change = 0
   
-  foodx = round(random.randrange(0, dis_width - snake_block) / 10.0)
-  foody = round(random.randrange(0, dis_width - snake_block) / 10.0)
+  foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0 # Controls x value location of food I think
+  foody = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0 # Controls y value location of food I think
 
   while not game_over:
+    
     while game_close == True:
       dis.fill(black) # Changes the background color
-      message("You Lost! Press Q to Quit or C to Play Again.", red)
+      message("You Lost! Press Q to Quit or C to Play Again", red)
       pygame.display.update()
     
       for event in pygame.event.get():
-        if. event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:
           if event.key == pygame.K_q: # If the user presses Q
             game_over = True
             game_close = False
@@ -55,8 +57,8 @@ def gameLoop(): # Creates a function
             gameLoop()
     
     for event in pygame.event.get():
-      if event.type == pygame.QUIT: # This line and the next might not be needed, they allow the user to close the screen when the mouse is over the quit button, but there's not quite button so im confused
-        game_over == True
+      if event.type == pygame.QUIT:
+        game_over = True
       if event.type == pygame.KEYDOWN: # When the user presses a direction key, the following code controls the snake's direction
         if event.key == pygame.K_LEFT:
           x1_change = -snake_block
@@ -72,7 +74,7 @@ def gameLoop(): # Creates a function
           x1_change = 0
   
     if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-      game_over = True
+      game_close = True
   
     x1 += x1_change
     y1 += y1_change
@@ -84,10 +86,6 @@ def gameLoop(): # Creates a function
     if x1 == foodx and y1 == foody:
       print("Yum!")
     clock.tick(snake_speed)
-
-  message("Game Over",red)
-  pygame.display.update()
-  time.sleep(2) # Game over screen stays for 2 seconds I think
 
   pygame.quit
   quit()
